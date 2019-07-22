7896715e059bcbe87bcd55948a2101baf3181a71
Safin Timur <timurtlt96@mail.ru>
2019-07-22 22:29:19 +0300

    Added tests for format_price

--- tests.py
+++ tests.py
@@ -1,1 +1,22 @@
-# TODO place your tests here
+import unittest
+from format_price import format_price
+
+
+class TestFormatPrice(unittest.TestCase):
+
+    def test_format_price(self):
+        self.assertEqual(format_price('3245.000000'), '3 245')
+        self.assertEqual(format_price('33245.000000'), '33 245')
+        self.assertEqual(format_price('333245.000000'), '333 245')
+        self.assertEqual(format_price('3333245.000000'), '3 333 245')
+
+        self.assertEqual(format_price('3245.500000'), '3 245.50')
+        self.assertEqual(format_price('3245.53450000'), '3 245.53')
+        self.assertEqual(format_price('3333245.53450000'), '3 333 245.53')
+
+        self.assertEqual(format_price(1234), '1 234')
+        self.assertEqual(format_price(1234.900), '1 234.90')
+
+
+if __name__ == '__main__':
+    unittest.main()
