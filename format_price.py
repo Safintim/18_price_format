5f76d372df5a442b20e42622406e4d18fa49d9ad
Safin Timur <timurtlt96@mail.ru>
2019-07-22 22:28:58 +0300

    Added format price

--- format_price.py
+++ format_price.py
@@ -1,5 +1,25 @@
+import argparse
+
+
+def main():
+    parser = create_parser()
+    namespace = parser.parse_args()
+    print(format_price(namespace.price))
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('price', help='price value')
+    return parser
+
+
 def format_price(price):
-    pass
+    price = float(price)
+    if int(price) == float(price):
+        return '{:,}'.format(int(price)).replace(',', ' ')
+    else:
+        return '{:,.2f}'.format(price).replace(',', ' ')
+
 
 if __name__ == '__main__':
-    pass
+    main()
