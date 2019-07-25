c6f5ed76b12542bc0993c4ec3b84252f91ffe86b
Safin Timur <timurtlt96@mail.ru>
2019-07-25 13:21:21 +0300

    Added more tests

--- tests.py
+++ tests.py
@@ -5,7 +5,8 @@ from format_price import format_price
 class TestFormatPrice(unittest.TestCase):
 
     def test_format_price_short_str(self):
-        self.assertEqual(format_price('0.0049'), '0.00')
+        self.assertEqual(format_price('0.0049'), '0')
+        self.assertEqual(format_price('3245.0000001'), '3 245')
         self.assertEqual(format_price('3245.000000'), '3 245')
         self.assertEqual(format_price('33245.000000'), '33 245')
         self.assertEqual(format_price('333245.000000'), '333 245')
@@ -29,7 +30,11 @@ class TestFormatPrice(unittest.TestCase):
         self.assertEqual(format_price(623401), '623 401')
 
     def test_format_price_float(self):
-        self.assertEqual(format_price(0.0049), '0.00')
+        self.assertEqual(format_price(3245.000001), '3 245')
+        self.assertEqual(format_price(3245.678901), '3 245.68')
+        self.assertEqual(format_price(3245.670001), '3 245.67')
+        self.assertEqual(format_price(1111.978901), '1 111.98')
+        self.assertEqual(format_price(0.0049), '0')
         self.assertEqual(format_price(1234.9), '1 234.90')
         self.assertEqual(format_price(3000.00), '3 000')
         self.assertEqual(format_price(10564.113), '10 564.11')
